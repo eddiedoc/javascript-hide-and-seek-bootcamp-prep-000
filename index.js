a13ea79aaf1e4f498fdebeb74fce99337438b84c
eddie <psyck_o@hotmail.com>
2018-01-09 06:42:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function getFirstSelector(selector) {
+  var firstElement = document.querySelector(selector);
+  
+  return firstElement;
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list");
+  
+  for(var i = 0; i < list.length;i++) {
+   
+   for(var j = 0;j<list[i].children.length;j++) {
+      list[i].children[j].innerHTML = parseInt(list[i].children[j].innerHTML) + n;
+   }
+  }
+}
+
+function deepestChild() {
+
+  var deep = document.querySelector("#grand-node");
+  var selector = "#grand-node";
+
+  do   {
+    deep = deep.querySelector(deep.children[0].tagName);
+     selector += ` ${deep.children[0].tagName}`;
+  } while(deep.children.length === 1)
+  
+return selector;  
+}
