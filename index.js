a432dba812fcd1efb6b3e7e15c56571dc3e93c33
eddie <psyck_o@hotmail.com>
2018-01-09 06:49:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function getFirstSelector(selector) {
+  var firstElement = document.querySelector(selector);
+  
+  return firstElement;
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list");
+  
+  for(var i = 0; i < list.length;i++) {
+   
+   for(var j = 0;j<list[i].children.length;j++) {
+      list[i].children[j].innerHTML = parseInt(list[i].children[j].innerHTML) + n;
+   }
+  }
+}
+
+function deepestChild() {
+
+  var deep = document.querySelector("#grand-node");
+  var selector = "#grand-node";
+
+  while(deep.children.length !== 0)   {
+    deep = deep.querySelector("div");
+     selector += " div";
+  } 
+  
+return document.querySelector(selector);  
+}
